5b45578fa1fe693903ec1d26fdd7246ab17ca0a2
Bjorn-Strom <bjorn.ivar.strom@bekk.no>
2019-11-27 14:41:32 +0100

    Run locally

--- migration/Migrations/2-addGuid.sql
+++ migration/Migrations/2-addGuid.sql
@@ -1,16 +1,16 @@
-DROP TABLE [Events];
+-- DROP TABLE [Events];
 
-CREATE TABLE [Events]
-(
-    Id UNIQUEIDENTIFIER PRIMARY KEY NOT NULL DEFAULT newid(),
-    Title VARCHAR(255) NOT NULL,
-    Description TEXT NULL,
-    Location VARCHAR(255) NOT NULL,
-    FromDate datetimeoffset(3) NOT NULL,
-    ToDate datetimeoffset(3) NOT NULL,
-    OrganizerEmail VARCHAR(255) NOT NULL,
-);
+--CREATE TABLE [Events]
+--(
+--    Id UNIQUEIDENTIFIER PRIMARY KEY NOT NULL DEFAULT newid(),
+--    Title VARCHAR(255) NOT NULL,
+--    Description TEXT NULL,
+--    Location VARCHAR(255) NOT NULL,
+--    FromDate datetimeoffset(3) NOT NULL,
+--    ToDate datetimeoffset(3) NOT NULL,
+--    OrganizerEmail VARCHAR(255) NOT NULL,
+--);
 
 
 INSERT INTO [Events]
-VALUES ('Test', 'Dette er et testarrangement', 'Skuret', '2019-11-07T10:00:00Z', '2019-11-07T10:30:00Z', 'morten@test.com');
+VALUES ('Test', 'Dette er et testarrangement', 'Skuret', '2019-11-07T10:00:00Z', '2019-11-07T10:30:00Z', 1388);
--- src/Events/Models.fs
+++ src/Events/Models.fs
@@ -9,22 +9,22 @@ open ArrangementService.Repo
 module Models =
 
     type DomainModel =
-        { Id: Guid
+        { Id: int
           Title: string
           Description: string
           Location: string
           FromDate: DateTimeOffset
           ToDate: DateTimeOffset
-          OrganizerEmail: string }
+          ResponsibleEmployee: int }
 
     type ViewModel =
-        { Id: Guid
+        { Id: int
           Title: string
           Description: string
           Location: string
           FromDate: DateTimeOffset
           ToDate: DateTimeOffset
-          OrganizerEmail: string }
+          ResponsibleEmployee: int }
 
     type WriteModel =
         { Title: string
@@ -32,13 +32,13 @@ module Models =
           Location: string
           FromDate: DateTimeOffset
           ToDate: DateTimeOffset
-          OrganizerEmail: string }
+          ResponsibleEmployee: int }
 
     type TableModel = ArrangementDbContext.dboSchema.``dbo.Events``
 
     type DbModel = ArrangementDbContext.``dbo.EventsEntity``
 
-    type Key = Guid
+    type Key = int
 
     let dbToDomain (dbRecord: DbModel): DomainModel =
         { Id = dbRecord.Id
@@ -47,7 +47,7 @@ module Models =
           Location = dbRecord.Location
           FromDate = dbRecord.FromDate
           ToDate = dbRecord.ToDate
-          OrganizerEmail = dbRecord.OrganizerEmail }
+          ResponsibleEmployee = dbRecord.ResponsibleEmployee }
 
     let updateDbWithDomain (db: DbModel) (event: DomainModel) =
         db.Title <- event.Title
@@ -55,7 +55,7 @@ module Models =
         db.Location <- event.Location
         db.FromDate <- event.FromDate
         db.ToDate <- event.ToDate
-        db.OrganizerEmail <- event.OrganizerEmail
+        db.ResponsibleEmployee <- event.ResponsibleEmployee
         db
 
     let domainToView (domainModel: DomainModel): ViewModel =
@@ -65,7 +65,7 @@ module Models =
           Location = domainModel.Location
           FromDate = domainModel.FromDate
           ToDate = domainModel.ToDate
-          OrganizerEmail = domainModel.OrganizerEmail }
+          ResponsibleEmployee = domainModel.ResponsibleEmployee }
 
     let writeToDomain (id: Key) (writeModel: WriteModel): DomainModel =
         { Id = id
@@ -74,7 +74,7 @@ module Models =
           Location = writeModel.Location
           FromDate = writeModel.FromDate
           ToDate = writeModel.ToDate
-          OrganizerEmail = writeModel.OrganizerEmail }
+          ResponsibleEmployee = writeModel.ResponsibleEmployee }
 
     let models: Models<DbModel, DomainModel, ViewModel, WriteModel, Key, TableModel> =
         { key = fun record -> record.Id
