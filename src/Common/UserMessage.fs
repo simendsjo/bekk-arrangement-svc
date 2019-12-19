49d272a2d036d1eee3c947720a14a55b7a5ab2ee
tarjei <tarjei.skjaerset@gmail.com>
2019-12-19 14:37:40 +0100

    Implement correct user error -> http error function

--- src/Common/UserMessage.fs
+++ src/Common/UserMessage.fs
@@ -12,5 +12,27 @@ module UserMessage =
         | NotFound of string
         | BadInput of string
 
+    type ErrorCodes =
+        | ResourceNotFound
+        | BadRequest
+        | InternalError
+
+
     let convertUserMessagesToHttpError (errors: UserMessage list): HttpError =
-        RequestErrors.BAD_REQUEST errors
+
+        let reduceErrors (errorCode, errorMessages) =
+            function
+            | NotFound errorMessage -> (ResourceNotFound, errorMessages @ [errorMessage])
+            | BadInput errorMessage -> 
+                let messages = errorMessages @ [errorMessage]
+                match errorCode with
+                | ResourceNotFound -> (ResourceNotFound, messages)
+                | _ -> (BadRequest, messages)
+
+        errors
+        |> List.fold reduceErrors (InternalError, [])
+        |> fun (errorCode, errorMessages) ->
+            match errorCode with
+            | ResourceNotFound -> RequestErrors.NOT_FOUND errorMessages
+            | BadRequest -> RequestErrors.BAD_REQUEST errorMessages
+            | InternalError -> ServerErrors.INTERNAL_ERROR "Something has gone wrong"
