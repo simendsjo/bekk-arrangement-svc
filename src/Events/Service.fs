303d29621e798fbea5e03d7bc8c429d16c137900
Karl Evald Wigestrand <karl.wigestrand@bekk.no>
2020-02-03 15:32:13 +0100

    Add create event email

--- src/Events/Service.fs
+++ src/Events/Service.fs
@@ -1,10 +1,12 @@
 namespace ArrangementService.Event
 
 open ArrangementService
-
+open ArrangementService.Email
 open ResultComputationExpression
 open Queries
 open UserMessages
+open ArrangementService.DomainModels
+open ArrangementService.DateTime
 
 module Service =
 
@@ -34,9 +36,45 @@ module Service =
                 return models.dbToDomain event
         }
 
+    let createCalendarAttachment (event: Event) (emailAddress: EmailAddress) =
+        [ "BEGIN:VCALENDAR"
+          "PRODID:-//Schedule a Meeting"
+          "VERSION:2.0"
+          "METHOD:REQUEST"
+          "BEGIN:VEVENT"
+          sprintf "DTSTART:%s" (toUtcString event.StartDate)
+          sprintf "DTSTAMP:%s" (System.DateTimeOffset.UtcNow.ToString())
+          sprintf "DTEND:%s" (toUtcString event.EndDate)
+          sprintf "LOCATION:%s" event.Location.Unwrap
+          sprintf "UID:%O" event.Id
+          sprintf "DESCRIPTION:%s" event.Description.Unwrap
+          sprintf "X-ALT-DESC;FMTTYPE=text/html:%s"
+              event.Description.Unwrap
+          sprintf "SUMMARY:%s" event.Title.Unwrap
+          sprintf "ORGANIZER:MAILTO:%s" event.OrganizerEmail.Unwrap
+          sprintf "ATTENDEE;CN=\"%s\";RSVP=TRUE:mailto:%s" emailAddress.Unwrap
+              emailAddress.Unwrap
+          "BEGIN:VALARM"
+          "TRIGGER:-PT15M"
+          "ACTION:DISPLAY"
+          "DESCRIPTION:Reminder"
+          "END:VALARM"
+          "END:VEVENT"
+          "END:VCALENDAR" ]
+        |> String.concat "\n"
+
+    let createEmail (event: Event) =
+        { Subject = sprintf "Du opprettet %s" event.Title.Unwrap
+          Message = "Hei.."
+          From = EmailAddress "brjilo@bekk.no"
+          To = event.OrganizerEmail
+          CalendarInvite = createCalendarAttachment event event.OrganizerEmail }
+
     let createEvent event =
         result {
             for newEvent in repo.create event do
+                let mail = createEmail newEvent
+                yield Service.sendMail mail
                 return newEvent
         }
 
