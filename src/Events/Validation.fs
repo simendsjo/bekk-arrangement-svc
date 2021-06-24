576e929be9d68ea0be59d6534491a17e8c730686
sarahjelle <sarahj.98@hotmail.com>
2021-06-24 15:41:10 +0200

    Enable editing macParticipants to 0.
    Co-authored-by: Tarjei Skjærset <tskj@tarjei.org>

--- src/Events/Validation.fs
+++ src/Events/Validation.fs
@@ -9,7 +9,12 @@ open ArrangementService.ResultComputationExpression
 module Validation =
     let assertNumberOfParticipantsLessThanOrEqualMax (event:Event) =
         result {
-            let! numberOfParticipants = Participant.Queries.getNumberOfParticipantsForEvent event.Id
-            if numberOfParticipants <= event.MaxParticipants.Unwrap then return () 
-            else return! Error [UserMessages.invalidMaxParticipantValue] 
+            let maxParticipants = event.MaxParticipants.Unwrap 
+            if maxParticipants = 0 then
+                return ()
+            else
+                let! numberOfParticipants = Participant.Queries.getNumberOfParticipantsForEvent event.Id
+
+                if numberOfParticipants <= maxParticipants then return () 
+                else return! Error [UserMessages.invalidMaxParticipantValue] 
         }
