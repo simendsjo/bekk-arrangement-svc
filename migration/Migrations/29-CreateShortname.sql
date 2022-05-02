6aebd773d607d5c5a25538fe794c20804e8f6749
Bjørn-Ivar Strøm <bjornivar.strom@gmail.com>
2022-05-02 15:35:15 +0200

    Move unique into migration of shortnames

--- migration/Migrations/29-CreateShortname.sql
+++ migration/Migrations/29-CreateShortname.sql
@@ -1,6 +1,2 @@
 USE [arrangement-db];
 ALTER TABLE [Events] ADD Shortname NVARCHAR (255) NULL DEFAULT NULL;
-
-CREATE UNIQUE NONCLUSTERED INDEX UQ_Event_Shortname
-    ON Events(Shortname)
-    WHERE Shortname IS NOT NULL;
--- migration/Migrations/30-migrateShortname.sql
+++ migration/Migrations/30-migrateShortname.sql
@@ -1,5 +1,9 @@
 USE [arrangement-db];
 
+CREATE UNIQUE NONCLUSTERED INDEX UQ_Event_Shortname
+    ON Events(Shortname)
+    WHERE Shortname IS NOT NULL;
+
 UPDATE
     [Events]
 SET
